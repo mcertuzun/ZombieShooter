56faa2032765c8d68c7a714f51caa1cbbd6beea3
MEHMET CAN ERTuZuN <mcan.ertuzun@tedu.edu.tr>
2019-11-21 22:08:02 +0300

    Eclipse version

--- src/main/Window.java
+++ src/main/Window.java
@@ -4,12 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Window extends Canvas {
+
     public Window(int width, int height, String title,Game game){
         JFrame window = new JFrame(title);
 
         window.setPreferredSize(new Dimension(width,height));
         window.setMaximumSize(new Dimension(width,height));
         window.setMinimumSize(new Dimension(width,height));
+
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setLocationRelativeTo(null);
